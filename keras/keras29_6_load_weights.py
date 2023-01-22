b44b9b769878036de4ca02232fecabc3fb934538
ConchK <121652343+ConchK@users.noreply.github.com>
2023-01-22 23:59:33 +0900

    가중치 save&load

---
+++ keras/keras29_6_load_weights.py
@@ -0,0 +1,64 @@
+from sklearn.datasets import load_boston
+from tensorflow.keras.models import Sequential, Model, load_model  #인풋 레이어를 명시해야 함.
+from tensorflow.keras.layers import Dense, Input
+from sklearn.model_selection import train_test_split
+import numpy as np
+path= './_save/'
+
+#1. 데이터
+datasets= load_boston()
+x=datasets.data
+y=datasets.target
+# print(x.shape, y.shape)   #(506, 13) (506,)
+
+x_train, x_test, y_train, y_test= train_test_split(
+        x,y, shuffle=True, random_state=333, test_size=0.2
+)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+scaler= MinMaxScaler()
+# scaler= StandardScaler()
+
+scaler.fit(x_train)
+x_train= scaler.transform(x_train)
+# == x_train= scaler.fit_transform(x_train)    #x범위 만큼의 가중치 생성
+
+x_test= scaler.transform(x_test)
+
+#2. 모델구성(함수형)  
+input1= Input(shape=(13,))
+dense1= Dense(1)(input1)
+dense2= Dense(15)(dense1)
+dense3= Dense(5)(dense2)
+output1= Dense(1)(dense3)
+model= Model(inputs=input1, outputs=output1)   #정의가 마지막으로 
+model.summary()
+# Total params: 130
+
+# model.save_weights(path+ 'keras29_5_save_weights1.h5')
+model.load_weights(path+ 'keras29_5_save_weights1.h5')
+#모델 저장 안되고 가중치만 저장 됨. weights만 저장 되기 때문에 모델, 컴파일을 명시 해야 함. 
+
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping    
+earlystopping= EarlyStopping(monitor='val_loss', mode='min', patience=10, restore_best_weights=True, verbose=1)  
+hist=model.fit(x_train, y_train, epochs=100, batch_size=1,
+        validation_split=0.2, callbacks=[earlystopping],  #반환값
+        verbose=1)
+#hist는 dictionary 형태로(key value) loss값이 들어있음/ 리스트 2개 이상
+
+# model.save_weights(path+ 'keras29_5_save_weights2.h5')
+model.load_weights(path+ 'keras29_5_save_weights2.h5')
+#모델 저장 안되고 가중치만 저장 됨. weights만 저장 되기 때문에 모델, 컴파일을 명시 해야 함. >> save_model 사용.
+
+#4. 평가, 예측
+loss=model.evaluate(x_test, y_test)
+print('loss :', loss)
+
+'''
+loss : 24.28295135498047
+'''
+
